3029f9293ca7b08b24e4fe57aed350859567c29a
Guillermo Rodriguez <gui_rod_dallas@hotmail.com>
2022-04-12 11:09:54 -0400

    Creation of outpule csv

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     DEFAULT_GROUP_END = 18 * 50
 
     try:
-        # Create data dictionary & histogram data entries
+        # Create data array for all nodes in landscape
         with open(_source_file, 'r') as _network_paths:
             for entry in _network_paths:
                 _source, _sink = int(entry.strip('\n').split('\t')[0]), int(entry.strip('\n').split('\t')[1])
@@ -29,7 +29,12 @@ if __name__ == "__main__":
 
         OUT_FOLDER = 'out'
         OUT_FILE = 'routes.txt'
-        
+
+        # Output desired nodes to local repository
+        with open(os.path.join(os.getcwd(), OUT_FOLDER, OUT_FILE), 'w') as _network_paths:
+            for entry in _actual_matrix:
+                _network_paths.write(str(entry[0]) + ',' + str(entry[1]) + '\n')
+
 
     except Exception as err:
             print(err)
